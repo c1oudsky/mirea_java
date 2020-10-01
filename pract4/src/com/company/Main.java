c769b438d411d501d9bcd6f2b066c06032d7b826
c1oudsky <70982846+c1oudsky@users.noreply.github.com>
2020-10-01 15:15:58 +0300

    Add files via upload

--- pract4/src/com/company/Main.java
+++ pract4/src/com/company/Main.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<EmployeePosition> operators = new ArrayList<EmployeePosition>();
-        for(int i=0; i<180; i++) { operators.add(new Operator(50000)); }
-        ArrayList<EmployeePosition> managers = new ArrayList<EmployeePosition>();
-        for(int i=0; i<80; i++) { managers.add(new Manager(60000)); }
-        ArrayList<EmployeePosition> topmanagers = new ArrayList<EmployeePosition>();
-        for(int i=0; i<10; i++) { topmanagers.add(new TopManager(70000)); }
+        Operator operator = new Operator(50000);
+        Manager manager = new Manager(60000);
+        TopManager topmanager = new TopManager(70000);
+        ArrayList<Employee> operators = new ArrayList<Employee>();
+        for(int i=0; i<180; i++) { operators.add(new Employee("Операторский", ("Операнд"+ (i+1) ), operator) ); }
+        ArrayList<Employee> managers = new ArrayList<Employee>();
+        for(int i=0; i<80; i++) { managers.add(new Employee("Управляющий",  ("Ментимер"+ (i+1) ), manager) ); }
+        ArrayList<Employee> topmanagers = new ArrayList<Employee>();
+        for(int i=0; i<10; i++) { topmanagers.add(new Employee("Топ",  ("Степан"+ (i+1) ), topmanager) ); }
         Company company = new Company();
         company.hireAll(operators); company.hireAll(managers); company.hireAll(topmanagers);
         System.out.println("13 самых высоких зарплат в нашей компании:");
